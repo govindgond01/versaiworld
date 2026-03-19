b45f7b0b813824350a1aa7fd32dff200c51f8756
abhishekgovind123 <gk468550@gmail.com>
2026-03-19 12:07:33 +0530

    routes path

--- frontend/src/pages/admin/SystemSettings.jsx
+++ frontend/src/pages/admin/SystemSettings.jsx
@@ -3,8 +3,9 @@ import { toast } from 'react-hot-toast';
 import {
   FiSettings, FiSave, FiShield, FiMail,
   FiBell, FiLock, FiUsers, FiGlobe, FiMoon, FiSun,
-  FiDollarSign, FiCalendar, FiClock,FiRefreshCw,FiPalette, FiServer,FiMenu 
+  FiDollarSign, FiCalendar, FiClock,FiRefreshCw,FiPalette, FiServer
 } from 'react-icons/fi';
+import { FiMenu } from 'react-icons/fi';
 import {
   MdOutlineSecurity, MdOutlineNotifications, MdOutlineLanguage,
   MdOutlineBackup, MdOutlinePayment
