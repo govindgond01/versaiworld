417aa87e4f44351a42751d7dd90d80a0e46974cc
abhishekgovind123 <gk468550@gmail.com>
2026-03-23 17:36:01 +0530

    FIX: course validation and default value

--- frontend/src/components/layouts/MainLayout.jsx
+++ frontend/src/components/layouts/MainLayout.jsx
@@ -30,7 +30,7 @@ const MainLayout = () => {
         </div>
         
         {/* Main Content Area */}
-        <div className="flex-1 md:ml-[324px] lg:ml-[332px] min-h-[calc(100vh-4rem)] bg-gray-100 py-2 px-4 md:pr-6 lg:pr-8">
+        <div className="flex-1 md:ml-[324px] lg:ml-[332px] min-h-[calc(100vh-4rem)] bg-gray-100 py-2 md:py-4 px-4 md:pr-6 lg:pr-8">
           <Outlet />
         </div>
       </div>
