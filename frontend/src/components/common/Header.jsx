3bbc7db44f67c8d76bf6746e78fd178121fbb77b
abhishekgovind123 <gk468550@gmail.com>
2026-03-24 10:51:57 +0530

    versailogo

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -294,8 +294,7 @@ const Header = ({ toggleSidebar }) => {
       <div className='h-16'>
         <div className='flex h-full items-center'>
           <div className='w-[300px] h-full border-r border-zinc-200 hidden md:flex items-center'>
-            
-            ' src={logoImg} alt="Logo" />
+            <img className='h-12' src={logoImg} alt="Logo" />
           </div>
 
           <div className='flex items-center md:hidden'>
